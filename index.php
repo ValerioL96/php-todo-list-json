3754238aab8f75c6093879a8548f6b5a1e8c5e54
ValerioL96 <v.lembo1396@gmail.com>
2024-07-02 16:41:10 +0200

    Added vuejs and bootstrap

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 Dobbiamo creare una web-app che permetta di leggere e scrivere una lista di Todo.
 Deve essere anche gestita la persistenza dei dati leggendoli da, e scrivendoli in un file JSON.
 Stack
-Html, CSS, VueJS (importato tramite CDN), axios, PHP-->$_COOKIE
+Html, CSS, VueJS (importato tramite CDN), axios, PHP-->
 
 
 <!DOCTYPE html>
@@ -11,10 +11,26 @@ Html, CSS, VueJS (importato tramite CDN), axios, PHP-->$_COOKIE
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP ToDo List JSON</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    rel="stylesheet" 
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+    crossorigin="anonymous">
+
 </head>
-<body>
+    <body>
+        <main id="app">
+
+
+        </main>
     
 
-<script src="./js/script.js"></script>
-</body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js"
+            integrity="sha512-JSCFHhKDilTRRXe9ak/FJ28dcpOJxzQaCd3Xg8MyF6XFjODhy/YMCM8HW0TFDckNHWUewW+kfvhin43hKtJxAw=="
+            crossorigin="anonymous" 
+            referrerpolicy="no-referrer">
+    </script>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="./js/script.js"></script>
+    </body>
 </html>
