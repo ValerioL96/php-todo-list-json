f61b4763164d9dea642b42ebf049ff0efc44ac5a
ValerioL96 <v.lembo1396@gmail.com>
2024-07-02 16:31:03 +0200

    Created files html and javascript

---
+++ index.php
@@ -0,0 +1,20 @@
+<!--Descrizione
+Dobbiamo creare una web-app che permetta di leggere e scrivere una lista di Todo.
+Deve essere anche gestita la persistenza dei dati leggendoli da, e scrivendoli in un file JSON.
+Stack
+Html, CSS, VueJS (importato tramite CDN), axios, PHP-->$_COOKIE
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP ToDo List JSON</title>
+</head>
+<body>
+    
+
+<script src="./js/script.js"></script>
+</body>
+</html>
